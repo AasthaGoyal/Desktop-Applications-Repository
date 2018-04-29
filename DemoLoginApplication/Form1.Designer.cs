caad76c93fe357fd07418f8344bef5c1824be7b5
Aastha Goyal <aastha2150@gmail.com>
2018-04-30 11:34:50 +1200

    Importing Project

---
+++ DemoLoginApplication/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace DemoLoginApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btmCancel = new System.Windows.Forms.Button();
+            this.btmLogin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserType = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dbAisDataSet = new DemoLoginApplication.dbAisDataSet();
+            this.tblusersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblusersTableAdapter = new DemoLoginApplication.dbAisDataSetTableAdapters.tblusersTableAdapter();
+            this.tableAdapterManager = new DemoLoginApplication.dbAisDataSetTableAdapters.TableAdapterManager();
+            this.tblusersDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbAisDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(34, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(313, 76);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(171, 20);
+            this.txtUserId.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(313, 113);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(171, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btmCancel
+            // 
+            this.btmCancel.Location = new System.Drawing.Point(227, 179);
+            this.btmCancel.Name = "btmCancel";
+            this.btmCancel.Size = new System.Drawing.Size(75, 23);
+            this.btmCancel.TabIndex = 6;
+            this.btmCancel.Text = "Cancel";
+            this.btmCancel.UseVisualStyleBackColor = true;
+            this.btmCancel.Click += new System.EventHandler(this.btmCancel_Click);
+            // 
+            // btmLogin
+            // 
+            this.btmLogin.Location = new System.Drawing.Point(409, 179);
+            this.btmLogin.Name = "btmLogin";
+            this.btmLogin.Size = new System.Drawing.Size(75, 23);
+            this.btmLogin.TabIndex = 7;
+            this.btmLogin.Text = "Login";
+            this.btmLogin.UseVisualStyleBackColor = true;
+            this.btmLogin.Click += new System.EventHandler(this.btmLogin_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "User Type:";
+            // 
+            // txtUserType
+            // 
+            this.txtUserType.Location = new System.Drawing.Point(313, 143);
+            this.txtUserType.Name = "txtUserType";
+            this.txtUserType.Size = new System.Drawing.Size(171, 20);
+            this.txtUserType.TabIndex = 9;
+            // 
+            // dbAisDataSet
+            // 
+            this.dbAisDataSet.DataSetName = "dbAisDataSet";
+            this.dbAisDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblusersBindingSource
+            // 
+            this.tblusersBindingSource.DataMember = "tblusers";
+            this.tblusersBindingSource.DataSource = this.dbAisDataSet;
+            // 
+            // tblusersTableAdapter
+            // 
+            this.tblusersTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.tblusersTableAdapter = this.tblusersTableAdapter;
+            this.tableAdapterManager.UpdateOrder = DemoLoginApplication.dbAisDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // tblusersDataGridView
+            // 
+            this.tblusersDataGridView.AutoGenerateColumns = false;
+            this.tblusersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblusersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.tblusersDataGridView.DataSource = this.tblusersBindingSource;
+            this.tblusersDataGridView.Location = new System.Drawing.Point(45, 238);
+            this.tblusersDataGridView.Name = "tblusersDataGridView";
+            this.tblusersDataGridView.Size = new System.Drawing.Size(384, 154);
+            this.tblusersDataGridView.TabIndex = 10;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "EmailId";
+            this.dataGridViewTextBoxColumn1.HeaderText = "EmailId";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Pass";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Pass";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "FirstName";
+            this.dataGridViewTextBoxColumn3.HeaderText = "FirstName";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "LastName";
+            this.dataGridViewTextBoxColumn4.HeaderText = "LastName";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 428);
+            this.Controls.Add(this.tblusersDataGridView);
+            this.Controls.Add(this.txtUserType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btmLogin);
+            this.Controls.Add(this.btmCancel);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbAisDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btmCancel;
+        private System.Windows.Forms.Button btmLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUserType;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private dbAisDataSet dbAisDataSet;
+        private System.Windows.Forms.BindingSource tblusersBindingSource;
+        private dbAisDataSetTableAdapters.tblusersTableAdapter tblusersTableAdapter;
+        private dbAisDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView tblusersDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ DemoLoginApplication/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoLoginApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void clearControls()
+        {
+            txtPassword.Text = " ";
+            txtUserId.Text = " ";
+            txtUserId.Focus();//means the focus should go to the username 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbAisDataSet.tblusers' table. You can move, or remove it, as needed.
+            this.tblusersTableAdapter.Fill(this.dbAisDataSet.tblusers);
+            clearControls();
+        }
+
+        private void btmLogin_Click(object sender, EventArgs e)
+        {
+
+            if(txtUserId.Text.Length < 2)
+            {
+                MessageBox.Show("Please enter correct id");
+                txtUserId.Focus();
+                return;
+            }
+            else if(txtPassword.Text.Length <4)
+            {
+                MessageBox.Show("Please enter correct password");
+                txtPassword.Focus();
+                return;
+            }
+
+            object  found = tblusersTableAdapter.ValidateUser(txtUserId.Text, txtPassword.Text);
+            if(found == null)
+            {
+                MessageBox.Show("Invalid id or password");
+                txtUserId.Focus();
+                return;
+                //return from this method
+            }
+            else
+            {
+                MessageBox.Show("Welcome to" + found);
+
+            }
+            clearControls();
+        }
+
+        private void btmCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
